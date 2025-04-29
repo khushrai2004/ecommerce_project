06a578c09d325e99149d5e60c5622edd6ea10e3b
rajeshtct96 <rajeshtct96@gmail.com>
2025-04-30 00:17:43 +0530

    add definitions for Product and CartItem entities

--- store/models.py
+++ store/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    description = models.TextField()
+    image = models.ImageField(upload_to='products/')
+    stock = models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.name
+
+
+class CartItem(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+
+    def total_price(self):
+        return self.quantity * self.product.price
